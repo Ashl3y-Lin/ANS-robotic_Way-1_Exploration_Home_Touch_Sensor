0839c6dbcd1f102cf7d69f9340fc9ef6116a2d73
Ashl3y-Lin <164275529+Ashl3y-Lin@users.noreply.github.com>
2024-05-14 14:24:43 -0600

    Add files via upload

---
+++ quizSection.js
@@ -0,0 +1,121 @@
+//a list of questions
+const questions = [
+    {
+        question: "Which are the interactions of using touch sensor?" ,
+        answers: [
+            {text: "Read and Pressed", correct: false}, 
+            {text: "Pressed and Released", correct: true}, 
+        ]
+    },
+    {
+        question: "How do you make the robot start moving AFTER touch sensor is [Pressed]?" ,
+        answers: [
+            {text: "1- Movement block. 2- Wait Until [pressed].", correct: false}, 
+            {text: "1- Wait Until [released]. 2- Movement block", correct: false}, 
+            {text: "1- Forever [pressed]. 2- Movement block", correct: false}, 
+            {text: "1- Wait Until [pressed]. 2- Movement block", correct: true}, 
+
+        ]
+    },
+    {
+        question:"What is the function of the touch sensor in EV3 robotics?" ,
+        answers: [
+            {text: "Detecting colors", correct: false}, 
+            {text: "Sensing temperature", correct: false}, 
+            {text: "Detecting physical contacts", correct: true}, 
+            {text: "Measuring distance", correct: false}, 
+        ]
+    },
+
+];
+
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons"); //This is the damn problem why i got stuck: answerElement instead of answerButton.
+const nextButton = document.getElementById("next-button");
+
+// To keep track of the scores and start the questions
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0; //when start the quiz, the question 1
+    score = 0;
+    nextButton.innerHTML = 'Next'; //display teh next button once you select an asnwer
+    showQuestion(); //calling the next question.
+}
+
+function showQuestion(){
+    resetState();
+
+    //display  the questions:
+    let currentQuestion = questions[currentQuestionIndex]; //keep track of the questions: begin with question 1 and continue.
+    
+    let questionNumber = currentQuestionIndex + 1; //next question, computer will know that the 1st question it's answered
+
+    questionElement.innerHTML = questionNumber + ". " + currentQuestion.question; //replace the text in the HTML to the texts from the list.
+
+
+    //displat the multiple questions asnwer texts
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button"); //in the button:....
+        button.innerHTML = answer.text; //get into the list: the answer element
+        button.classList.add("btn");
+        answerButton.appendChild(button); //Thios is to append a child element to an existing parent element
+        
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+
+}
+
+function resetState(){ //this function it's to remove the asnwers 1 answer 2 ... from the html
+    nextButton.style.display = "none";
+    while(answerButton.firstChild){
+        answerButton.removeChild(answerButton.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }else{
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButton.children).forEach(button =>{
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex < questions.length){ //the amount of questions I have.
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+})
+startQuiz();
